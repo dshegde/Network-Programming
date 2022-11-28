46e1d845b65f0f166a5af86dad93cbc86ab2ea05
Deepa Hegde <hegde@pdx.edu>
2022-11-27 18:53:37 -0800

    generating/adding config file

---
+++ Internet-Relay-Chat/build_app_config.py
@@ -0,0 +1,12 @@
+import configparser
+
+config = configparser.ConfigParser()
+
+''' Builds structure for the INI file that is going to be generated'''
+config.add_section('Network Connection')
+config.set('Network Connection', 'HOST', '127.0.0.1')
+config.set('Network Connection', 'PORT', '65000')
+
+'''Writes the structure to the new INI file in the given location'''
+with open(r'app_config_file.ini', 'w') as configfile:
+    config.write(configfile)
